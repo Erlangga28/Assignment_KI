87f90940732e3b91e3cc9e5f677f8425545509d5
Erlangga Wahyu Utomo <erlanggawahyu21@gmail.com>
2023-11-21 00:25:20 +0700

    created

---
+++ Resource/function.php
@@ -0,0 +1,68 @@
+<?php
+// Koneksi Database
+$koneksi = mysqli_connect("localhost", "root", "", "db_masyarakat");
+
+// membuat fungsi query dalam bentuk array
+function query($query)
+{
+    // Koneksi database
+    global $koneksi;
+
+    $result = mysqli_query($koneksi, $query);
+
+    // membuat varibale array
+    $rows = [];
+
+    // mengambil semua data dalam bentuk array
+    while ($row = mysqli_fetch_assoc($result)) {
+        $rows[] = $row;
+    }
+
+    return $rows;
+}
+
+// Membuat fungsi tambah
+function tambah($data)
+{
+    global $koneksi;
+
+    $nik = htmlspecialchars($data['nik']);
+    $nama = htmlspecialchars($data['nama']);
+    $alamat = htmlspecialchars($data['alamat']);
+    $gol_darah = htmlspecialchars($data['gol.darah']);
+    
+
+    $sql = "INSERT INTO masyarakat(nik, nama, alamat, gol.darah) VALUES ('$nik','$nama','$alamat','$gol_darah')";
+
+    mysqli_query($koneksi, $sql);
+
+    return mysqli_affected_rows($koneksi);
+}
+
+// Membuat fungsi hapus
+function hapus($nik)
+{
+    global $koneksi;
+
+    mysqli_query($koneksi, "DELETE FROM masyarakat WHERE nik = $nik");
+    return mysqli_affected_rows($koneksi);
+}
+
+// Membuat fungsi ubah
+function ubah($data)
+{
+    global $koneksi;
+
+    $nik = htmlspecialchars($data['nim']);
+    $nama = htmlspecialchars($data['nama']);
+    $alamat = htmlspecialchars($data['alamat']);
+    $gol_darah = htmlspecialchars($data['gol.darah']);
+    
+
+    $sql = "UPDATE masyarakat SET nama = '$nama', alamat = '$alamat', gol.darah = '$gol_darah' WHERE nik = $nik";
+
+    mysqli_query($koneksi, $sql);
+
+    return mysqli_affected_rows($koneksi);
+}
+
---
+++ db_masyarakat.sql
@@ -0,0 +1,99 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Nov 20, 2023 at 06:23 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_masyarakat`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `password` varchar(50) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id`, `username`, `password`) VALUES
+(1, 'admin', 'admin');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `masyarakat`
+--
+
+CREATE TABLE `masyarakat` (
+  `id` int(2) NOT NULL,
+  `nama` varchar(250) NOT NULL,
+  `nik` varchar(20) NOT NULL,
+  `alamat` varchar(250) NOT NULL,
+  `gol_darah` varchar(2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `masyarakat`
+--
+
+INSERT INTO `masyarakat` (`id`, `nama`, `nik`, `alamat`, `gol_darah`) VALUES
+(1, 'ERlangga', '356472933', 'salak no69', 'O');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `masyarakat`
+--
+ALTER TABLE `masyarakat`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `masyarakat`
+--
+ALTER TABLE `masyarakat`
+  MODIFY `id` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
